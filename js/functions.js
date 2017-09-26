6e09e03ca9aed70bb31d95e22ebe467a3dfd744c
Algorun <robinbigaut777@gmail.com>
2017-09-26 17:59:37 +0200

    Update sources
    Mise à jour de l'architecture du porjet

---
+++ js/functions.js
@@ -0,0 +1,13 @@
+/*
+	N'écrivez PAS vos fonctions JS ici.
+	À la place, créez un autre fichier de cette façon :
+
+	'votrePrenom_functions.js'
+
+	---> Attention, pensez au camelCase et à l'indentation !
+	---> Tout code mal fait ne sera pas integré et vous sera renvoyé illico ;)
+
+	Les fonctions integrées seront ensuite déplacées dans ce fichier, donc n'écrivez absolument rien ici,
+	mais pensez à TOUJOURS intégrer ce fichier dans vos fichiers
+	(ex: <script src="js/functions.js"></script>)
+*/
